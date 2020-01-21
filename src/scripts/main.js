777d821eb9d751311107c7de1f321a7f977e83f7
Tyler Simeone <tyler.simeone@gmail.com>
2020-01-21 14:04:07 -0600

    found solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,21 +1,23 @@
-const articleEl = document.querySelector("#messages");
+// NEED TO MAKE FN TO RETURN SECTION DOM COMPONENT FIVE TIMES
+// CAN MAKE LOOP TO RUN THE FN FIVE TIMES
+// THEN ATTACH EACH SECTION COMPONENT TO ITS PARENT CONTAINER VIA 
+// document.appendChild();
 
-let sectionEl_1 = document.createElement("section");
-let sectionEl_2 = document.createElement("section");
-let sectionEl_3 = document.createElement("section");
-let sectionEl_4 = document.createElement("section");
-let sectionEl_5 = document.createElement("section");
 
-sectionEl_1.className = "message";
-sectionEl_2.className = "message";
-sectionEl_3.className = "message";
-sectionEl_4.className = "message";
-sectionEl_5.className = "message";
 
-articleEl.appendChild(sectionEl_1);
-articleEl.appendChild(sectionEl_2);
-articleEl.appendChild(sectionEl_3);
-articleEl.appendChild(sectionEl_4);
-articleEl.appendChild(sectionEl_5);
 
-console.log(articleEl);
+let articleEl = document.querySelector("#messages");
+
+// Creating DOM ele depending on arguments
+const sectionComponent = (element, className, content) => {
+    return `<${element} class="${className}">${content}</${element}>`
+}
+
+// Adding DOM component to parent container 5 times
+const renderDomComponent = content => {
+    articleEl.innerHTML += sectionComponent("section", "message", content);
+}
+
+renderDomComponent("Hello World!");
+renderDomComponent("Awesomesauce");
+renderDomComponent("Excellent");
